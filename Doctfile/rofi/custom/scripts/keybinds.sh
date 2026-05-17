66dd2cb2e4ebc2afb7d06b4c6bf1cbcf0c765f18
Zerphys571 <vianaguilherme2013@gmail.com>
2026-05-17 03:37:18 -0400

    Refactor keybinds.sh to include modifier keys

--- Doctfile/rofi/custom/scripts/keybinds.sh
+++ Doctfile/rofi/custom/scripts/keybinds.sh
@@ -6,4 +6,6 @@ ROFI_THEME="${HOME}/.config/rofi/custom/style-5.rasi"
 export HYPRLAND_INSTANCE_SIGNATURE=$HYPRLAND_INSTANCE_SIGNATURE
 
 #--------COMANDO-------------
-hyprctl -j binds | jq -r '.[] | select(.repeat == false and .mouse == false) | "        +    \(.key)       \(if .description != "" then .description else .arg end)"'
+hyprctl -j binds | jq -r '.[] |
+  select(.repeat == false and .mouse == false) |
+  "        + \(if .modmask == 65 then "SHIFT + " elif .modmask == 68 then "CTRL + " elif .modmask == 72 then "ALT + " else "" end)\(.key)       \(if .description != "" then .description else .arg end)"'
